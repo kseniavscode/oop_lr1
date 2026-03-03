186035f0af8cf53a98ebb63756c0a24c1a4b1715
ksenia <ms.ks2007@mail.ru>
2026-03-03 12:07:29 +0300

    Added abstract class User

---
+++ LR1/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR1.Models
+{
+    internal abstract class User
+    {
+        private Guid Id { get; set; }
+        private string Name { get; set; }
+        private string Password { get; set; }
+        private Role Role { get; set; }
+
+        public User(string name, string password, Role role)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Password = password;
+            Role = role;
+        }
+    }
+
+    enum Role
+    {
+        Client,
+        Manager,
+        Admin
+    }
+}
